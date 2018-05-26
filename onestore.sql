77a58a15b6f3758e83663b2937523bfec02c2891
borislav333 <35341004+borislav333@users.noreply.github.com>
2018-05-26 08:24:24 +0300

    Add files via upload

---
+++ onestore.sql
@@ -0,0 +1,25 @@
+-- --------------------------------------------------------
+-- Хост:                         127.0.0.1
+-- Версия на сървъра:            5.7.19 - MySQL Community Server (GPL)
+-- ОС на сървъра:                Win64
+-- HeidiSQL Версия:              9.5.0.5263
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Data exporting was unselected.
+-- Data exporting was unselected.
+-- Data exporting was unselected.
+-- Data exporting was unselected.
+-- Data exporting was unselected.
+-- Data exporting was unselected.
+-- Data exporting was unselected.
+-- Data exporting was unselected.
+-- Data exporting was unselected.
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
