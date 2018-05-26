ddb529b852f8b9bfbfe3d86f7559e1f7744b5f9a
Borislav <borislavqkimov@yahoo.com>
2018-05-26 07:59:23 +0300

    New Store

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,25 +1,46 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Borislav
- * Date: 21.4.2018 г.
- * Time: 10:30 ч.
- */
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Product;
+use App\Form\CartType;
+use function Sodium\crypto_pwhash;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\HttpFoundation\Cookie;
 
-class DefaultController extends AbstractController
+class HomeController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/", name="home")
      */
-    public function index()
+    public function index(AuthorizationCheckerInterface $authChecker,Request $request)
     {
+        if ((false === $authChecker->isGranted('ROLE_USER')) || (false === $authChecker->isGranted('ROLE_ADMIN'))) {
 
+            /*$cookies=$request->cookies->get('PHPSESSID');
+            $cookiesMD5=md5($cookies);
+            $request->getSession()->set('GUEST_SESS',$cookiesMD5);
 
-        return $this->render('index.html.twig');
+            var_dump();*/
+        }
+
+        $product=$this->getDoctrine()->getRepository(Product::class)->findAll();
+        $session=$request->getSession();
+        $cartProducts=$session->get('products');
+        //var_dump($request->request->get('add'));
+
+        //count($cartProducts);
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+            'product'=>$product,'cartProducts'=>$cartProducts
+        ]);
     }
+
+
 }
